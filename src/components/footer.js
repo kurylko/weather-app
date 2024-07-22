635faf299cfa4227b523c6f733c4020a2e4e506d
kurylko <reviliya@gmail.com>
2024-07-22 18:35:11 +0200

    header, footer, fiveicon placed

---
+++ src/components/footer.js
@@ -0,0 +1,8 @@
+import React from "react"
+
+const Footer = () => {
+    return(
+        <footer>Footer is here</footer>
+    );
+}
+export default Footer;
---
+++ src/components/header.js
@@ -0,0 +1,8 @@
+import React from "react"
+
+const Header = () => {
+    return(
+        <header>Header is here</header>
+    );
+}
+export default Header;
