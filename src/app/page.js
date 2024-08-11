085ad344c12b525f6384f7fed0764165e6a2b334
kurylko <reviliya@gmail.com>
2024-08-11 14:47:00 +0200

    api fixed

--- src/app/page.js
+++ src/app/page.js
@@ -7,15 +7,15 @@ export default function Home() {
 
     const  {location, error: locationError} = useUserLocation();
     const { latitude, longitude } = location;
-    const {currentWeather, error: currentWeatherError} = useCurrentWeather();
+    const {currentWeather, error: currentWeatherError} = useCurrentWeather(latitude, longitude);
 
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
       </div>
-        {location ? <p> {location.latitude} </p> : <p> No location detected </p>}
-        {location ? <p> {currentWeather} </p> : <p> No weather detected </p>}
+        {location ? <p> {`User's location: ${location.latitude}, ${location.longitude}`} </p> : <p> No location detected </p>}
+        {location ? <p> {`Current weather: ${currentWeather}`} </p> : <p> No weather detected </p>}
     </main>
   );
 }
