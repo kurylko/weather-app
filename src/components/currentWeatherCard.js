da0eb657141eecb7aa96d64270ab74f8de5d5a30
kurylko <reviliya@gmail.com>
2024-08-13 12:08:17 +0200

    formatted local date format

--- src/components/currentWeatherCard.js
+++ src/components/currentWeatherCard.js
@@ -1,4 +1,5 @@
 import React from "react"
+import {format} from "date-fns";
 
 const CurrentWeatherCard = ({currentWeather}) => {
     if (!currentWeather || !currentWeather.current) {
@@ -7,10 +8,17 @@ const CurrentWeatherCard = ({currentWeather}) => {
         )
     } else {
 
+        const dateString = currentWeather.location.localtime;
+        const date = new Date (dateString);
+        const formattedDate = format(date, 'eeee, MMMM d, yyyy h:mm a');
+
+        console.log(formattedDate);
+
         return (
             <div class='flex-col items-center w-400 bg-zinc-200 rounded-lg m-10 p-10'>Current weather card
-                <div>{`${currentWeather.location.name}, ${currentWeather.location.region}`}</div>
+                <div>{`${currentWeather.location.name}, ${currentWeather.location.region} (${currentWeather.location.country})`}</div>
                 <div>{currentWeather.current.condition.text}</div>
+                <div>{formattedDate}</div>
             </div>
         );
     }
