81f02a92a01b4bcbd7c5f1879ca8c629cf53f1c8
kurylko <reviliya@gmail.com>
2024-08-22 23:39:24 +0200

    search component upd

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -1,9 +1,32 @@
-const SearchBar = ({searchInput, handleSearch, handleChangeSearch}) => {
+import {getCoordinates} from "@/state/slice";
+import {useState} from 'react';
+import { useDispatch} from 'react-redux';
+
+const SearchBar = () => {
+    const dispatch = useDispatch()
+
+    const [searchInput, setSearchInput] = useState('');
+
+    const handleChangeSearch = (e) => {
+        e.preventDefault();
+        setSearchInput(e.target.value);
+    }
+
+    const handleSearch = () => {
+        dispatch(getCoordinates({city: searchInput}))
+    }
+
+    const handleKeyDown = (event) => {
+        if (event.key === 'Enter') {
+            handleSearch();
+        }
+    };
+
 
     return (
         <div className='search-bar'>
             <input className='search-input' type='text' placeholder='Type the location' value={searchInput}
-                   onChange={handleChangeSearch}></input>
+                   onChange={handleChangeSearch} onKeyDown={handleKeyDown}></input>
             <button className='search-button' onClick={handleSearch}>Search</button>
         </div>
     );
