30c20c7cf0fe0ded50c41517f51dd5a37e320e3c
kurylko <reviliya@gmail.com>
2024-08-23 16:41:40 +0200

    getting hourly weather for 0, 6, 12, 18 hours

--- src/components/HourlyWeatherCard.js
+++ src/components/HourlyWeatherCard.js
@@ -3,10 +3,16 @@ const HourlyWeatherCard = ({forecast}) => {
         return;
     }
 
-    const hourlyWeather = forecast.forecast['forecastday'][0][''];
+    const hourlyWeatherData = forecast.forecast['forecastday'][0]['hour'];
+
+    const hourlyWeatherDataForSpecificHours = [0, 6, 12, 18].map((hourIndex => hourlyWeatherData[hourIndex]));
+
+    console.log('hourlyWeather:', hourlyWeatherDataForSpecificHours);
 
     return (
-        <div className='hourly-weather-card'>hourly </div>
+        <div className='hourly-weather-card'>hourly
+            <div></div>
+        </div>
     )
 }
 
