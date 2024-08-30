be34847d2517761a15c9dc0e53d2daa1c53b6822
kurylko <reviliya@gmail.com>
2024-08-30 20:10:42 +0200

    cleaned unused

--- src/hooks/useUserLocation.js
+++ src/hooks/useUserLocation.js
@@ -4,23 +4,28 @@ import {parsePlaceData} from "@/utils/parsePlaceData";
 const useUserLocation = () => {
     const [geoLocationData, setGeoLocationData] = useState(null);
     const [geoLocationError, setGeoLocationError] = useState(null);
+    const [loading, setLoading] = useState(true);
+
 
     useEffect(() => {
         if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition(
                 (position) => {
+                    setLoading(false);
                     setGeoLocationData(parsePlaceData({placeData: position?.coords}));
                 },
                 (error) => {
+                    setLoading(false);
                     setGeoLocationError('Oops.Can not get your geolocation');
                 }
             );
         } else {
+            setLoading(false);
             setGeoLocationError('Your geolocation is not supported by your browser');
         }
     }, []);
 
-    return {geoLocationData, geoLocationError};
+    return {geoLocationData, geoLocationError, geoLocationLoading: loading};
 }
 
 export default useUserLocation;
