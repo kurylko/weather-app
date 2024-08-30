c564d9748127c974753ec68e7a03ff1789ffb414
kurylko <reviliya@gmail.com>
2024-08-30 13:26:34 +0200

    search from start page working

--- src/app/no-geoData/page.js
+++ src/app/no-geoData/page.js
@@ -1,21 +1,49 @@
-'use client';
-import Image from "next/image";
-import searchIcon from "../../../public/icons/search.png";
+'use client'
 import {useRouter} from "next/navigation";
+import {useDispatch} from "react-redux";
+import {getCoordinates} from "@/state/searchPlaceSlice";
+import SearchBar from "@/components/SearchBar";
+import {useRef, useState} from "react";
 
-export default async function NoGeoDataPage() {
+export default function NoGeoDataPage() {
     const router = useRouter();
+
+    const dispatch = useDispatch()
+
+    const [searchInput, setSearchInput] = useState('');
+    const inputRef = useRef(null);
+
+    const handleChangeSearch = (e) => {
+        e.preventDefault();
+        setSearchInput(e.target.value);
+    }
+
+    const handleSearch = () => {
+        dispatch(getCoordinates({city: searchInput}));
+        setSearchInput('');
+        if (inputRef.current) {
+            inputRef.current.blur();
+            router.push('/weather');
+        }
+    }
+
+    const handleKeyDown = (event) => {
+        if (event.key === 'Enter') {
+            handleSearch();
+            setSearchInput('');
+            if (inputRef.current) {
+                inputRef.current.blur();
+                router.push('/weather');
+            }
+        }
+    };
+
 
     return (
         <div className='no-user-geolocation-page'>
-            <span className='no-geolocation-text'>Can`t get your geolocation. Please, type the location in search to see the weather forecast.</span>
-            <div className='search-bar'>
-                <div className='search-input-wrapper'>
-                    <input className='search-input' type='text' placeholder='Type the location'></input>
-                    <Image src={searchIcon} alt="search icon" className="search-icon"/>
-                </div>
-                <button className='search-button' onClick={() => router.push('/weather')}>Search</button>
-            </div>
+            <span className='no-geolocation-text'>Please, type the location in search to see the weather forecast.</span>
+            <SearchBar onClick={handleSearch} onKeyDown={handleKeyDown} onChange={handleChangeSearch}
+                       searchInput={searchInput} inputRef={inputRef}/>
         </div>
     );
 }
